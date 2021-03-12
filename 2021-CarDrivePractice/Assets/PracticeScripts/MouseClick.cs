8da6958ef730fa9d389f8efd7f32ca45f772f036
ryanb <boydstunryan@yahoo.com>
2021-03-11 19:52:37 -0700

    Lotsa practice and changed project name

---
+++ 2021-CarDrivePractice/Assets/PracticeScripts/MouseClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Rigidbody.AddForce(-transform.forward * 500f);
+        Rigidbody.useGravity = true;
+    }
+}
